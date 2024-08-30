aef2f3035ca5530ef5260c3fbaea429fd1ac7fb8
Dylan Hutson <dylphuts@ut.utm.edu>
2024-08-30 12:09:38 -0500

    made a Hello World Script

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file main.cpp
+ * @author Dylan Hutson
+ * @date 2024-08-30
+ * @brief a hello wolrd
+ * 
+ * A test of getting everything set up for this class
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout<<"Hello World!"<<endl;
+}
